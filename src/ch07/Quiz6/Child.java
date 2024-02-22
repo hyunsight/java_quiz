e85a8cc7f6a739f4560063808deea49f36a77227
hyunsight <blueskytjgus@gmail.com>
2024-02-22 17:54:56 +0900

    Quiz6 주석 추가

--- src/ch07/Quiz6/Child.java
+++ src/ch07/Quiz6/Child.java
@@ -7,7 +7,7 @@ public class Child extends Parent {
         //원인: 부모 클래스의 생성자에 매개변수가 있는 상태에서 super()가 생략되어 발생
         //수정: super() 추가
         super(name); 
-        this.name = name;
+        //this.name = name;
         this.studentNo = studentNo;
     }
 }
