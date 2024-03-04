bc7d5474d7fd80b93a60a41f3006bd816e2ddd13
hyunsight <blueskytjgus@gmail.com>
2024-03-04 14:56:48 +0900

    ch11

---
+++ src/ch11/quiz6/Quiz6.java
@@ -0,0 +1,20 @@
+package ch11.quiz6;
+
+public class Quiz6 {
+
+    String[] strArray = {"10", "2a"};
+    int value = 0;
+//
+//for (int i = 0; i <= 2 ; i++) {
+//        try {
+//            value = Integer.parseInt(strArray[i]);
+//        } catch (ArrayIndexOutOfBoundsException e) {
+//            System.out.println("인덱스를 초과했음"); //배열은 총 1까지 있으나, i = 2까지 포함되므로 인덱스 초과 에러 출력
+//        } catch (NumberFormatException e) {
+//            System.out.println("숫자로 변환할 수 없음"); //"2a"는 숫자로 변환할 수 없으므로 숫자 변환 불가 에러 출력
+//        } finally {
+//            System.out.println(value);
+//        }
+//    }
+
+}
---
+++ src/ch11/quiz7/NotExistIDException.java
@@ -0,0 +1,9 @@
+package ch11.quiz7;
+
+public class NotExistIDException extends Exception {
+    public NotExistIDException() {}
+
+    public NotExistIDException(String message) {
+        super(message);
+    }
+}
