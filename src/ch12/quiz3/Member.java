3e4460494d2b4eef8349b76620c0aa92928327c8
hyunsight <blueskytjgus@gmail.com>
2024-03-06 13:49:12 +0900

    ch12 추가

--- src/ch12/quiz3/Member.java
+++ src/ch12/quiz3/Member.java
@@ -9,8 +9,9 @@ public class Member {
         this.name = name;
     }
 
+    //멤버(객체) 계속 추가해도 오버라이딩 통해 자동 출력 가능
     @Override
     public String toString() {
-        return name = "이파란";
+        return id + ": " + name;
     }
 }
--- src/ch12/quiz6/Quiz6.java
+++ src/ch12/quiz6/Quiz6.java
@@ -4,8 +4,9 @@ import java.util.StringTokenizer;
 
 public class Quiz6 {
     public static void main(String[] args) {
+        //(참조) split의 경우, 배열 잘라낼 때 사용
         String str = "아이디, 이름, 패스워드";
-        StringTokenizer str2 = new StringTokenizer(str);
+        StringTokenizer str2 = new StringTokenizer(str, ",");
         while (str2.hasMoreTokens()) {
             String token = str2.nextToken();
             System.out.println(token);
